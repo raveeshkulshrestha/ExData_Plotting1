68020ed946781c8be8114000e0e0538a0f736265
raveeshkulshrestha <72356048+raveeshkulshrestha@users.noreply.github.com>
2021-02-02 16:28:06 +0100

    Adding the code for the second plot.

---
+++ plot2.R
@@ -0,0 +1,34 @@
+pwdf <- read.table(file = "./household_power_consumption.txt", sep = ";", header = TRUE)
+
+## Subset the data set within the date range
+validrangedf <- subset(pwdf, as.Date(pwdf$Date, format = "%d/%m/%Y") >= "2007-02-01" & 
+                         as.Date(pwdf$Date, format = "%d/%m/%Y") <= "2007-02-02")
+
+
+## The below also works.
+
+##validrangedf <- subset(pwdf, as.Date(pwdf$Date, format = "%d/%m/%Y") >= 
+##                         as.Date("2007-02-01", format = "%Y-%m-%d") & 
+##                         as.Date(pwdf$Date, format = "%d/%m/%Y") <= 
+##                         as.Date("2007-02-02", format = "%Y-%m-%d"))
+## dimension for both is 2880    9
+
+## The below range gives the weekdays of the selected subset - Thursday and 
+## Friday which is what we want - and will be the xlab of the graph.
+##range(weekdays(as.Date(validrangedf$Date, format = "%d/%m/%Y")))
+
+## Y axis is Global Active Power
+## X axis is time series for which we need to combine data and time together
+
+## Creata a new column datetime for x axis
+validrangedf$DateTime <- paste(as.Date(validrangedf$Date, format = "%d/%m/%Y"), 
+                               validrangedf$Time) 
+
+## plot the graph
+plot(strptime(validrangedf$DateTime, format = '%Y-%m-%d %H:%M:%S'), 
+     validrangedf$Global_active_power, type = "S",ylab = "Global Active Power (kilowatts)", xlab="")
+##with(validrangedf, plot(strptime(validrangedf$DateTime, format = '%Y-%m-%d %H:%M:%S'), Global_active_power))
+
+dev.copy(png, file="plot2.png", width = 480, height = 480,units = "px")
+dev.off()
+
